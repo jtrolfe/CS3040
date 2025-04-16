ace0e48c3f8702e0b93cf637fccd11151b71b63b
John Rolfe <jtrolfe@gmail.com>
2025-04-16 08:08:23 -0700

    Removed non useful comments

--- proj1/asciitotal.c
+++ proj1/asciitotal.c
@@ -12,19 +12,6 @@
 // Example: ./asciitotal hello 5
 // ------------------------------------------------------
 
-/*
-1. verify number of arguments
-
-2. convert arguments into numbers 
-    use errno, strlen(does not set errno), 
-    loop through argv[2] test if isdigit
-    use strtolon(does set errno)
-    verify number is within bounds (1-100)
-
-3. convert argv[1] to a numerical value using ascii codes
-    use a loop
-
-*/
 
 #include<stdlib.h>
 #include<stdio.h>
